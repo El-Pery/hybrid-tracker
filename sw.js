583b8b208062a71a8234063da76ecd6d40a69e74
El-Pery <paseolpz@gmail.com>
2026-08-18 02:02:55 +0200

    Añadido pestaña mantenimientos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hybrid-tracker-v1.1';
+const CACHE_NAME = 'hybrid-tracker-v1.2';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,6 +9,20 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
